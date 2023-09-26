705f341a53ccfaba7b061aa9d7fb223fe5029ffd
Nicolas Castellanos <nicolas.castellanos012@gmail.com>
2023-09-25 19:39:46 -0500

    agregue mas info

--- lista.js
+++ lista.js
@@ -1,27 +1,35 @@
-let miLista =
-    ['Juan Castillo',
-        'Miguel Martinez',
-        'Maria Gomez']
-function agregarNuevoContacto(contacto) {
-    miLista.push(contacto);
-    console.log('Contacto agregado ' + contacto);
-}
-console.log(miLista);
-const borrarContacto = (contacto) => {
-    let indice = miLista.indexOf(contacto);
-    if (indice !== -1) {
-        miLista.splice(indice, 1);
-        console.log('Contacto borrado: ' + contacto);
-    } else {
-        console.log('No se encontro ese contacto');
+const listaContactos = [
+    {
+        id: 1224,
+        nombre: "sergio",
+        apellido: "Sanchez",
+        telefono: 124136231,
+        ubicacion: { ciudad: "Colombia", direccion: "2t2466" },
+    },
+    {
+        id: 13434,
+        nombre: "Mario",
+        apellido: "Castañeda",
+        telefono: 324745141,
+        ubicacion: { ciudad: "Guatemala", direccion: "232gwd42" },
+    },
+    {
+        id: 65432,
+        nombre: "Mario",
+        apellido: "Martinez",
+        telefono: 12423323521,
+        ubicacion: { ciudad: "si", direccion: "2t2qwfq6" },
+    },
+];
+function agregarContacto(id, nombre, apellido, telefono, ubicacion) {
+    const nuevoContacto = {
+        id: id,
+        nombre: nombre,
+        apellido: apellido,
+        telefono: telefono,
+        ubicacion: ubicacion,
     }
-}
-const mostrarLista = () => {
-    for (const i of miLista) {
-        console.log('Estos son los usuarios: ' + i);
-    }
-}
-agregarNuevoContacto('Mauricio Cardenas');
-agregarNuevoContacto('Pepito Fernandez');
-borrarContacto('Mauricio Cardenas');
-mostrarLista(miLista);
+    listaContactos.push(nuevoContacto);
+};
+agregarContacto(121241, "mario", "paorew", 13294102951, ubicacion = "colombia", ubicacion = "ig293ug");
+console.log(listaContactos);
